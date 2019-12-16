0d5982fba928360387e499b4c2a7d2f81f6e62c4
donghao02 <donghao02@baidu.com>
2019-12-16 16:19:08 +0800

    update

--- reader.go
+++ reader.go
@@ -3,10 +3,13 @@ package rediscli
 import (
 	"errors"
 	"fmt"
-    "strings"
-    "strconv"
+	"strconv"
+	"strings"
 )
 
+/*
+ * read line from server
+ */
 func ReadLine(ctx *RedisContext) string {
 	res, err := ctx.reader.ReadString('\n')
 	if err != nil {
@@ -24,52 +27,63 @@ func ReadReply(context *RedisContext) (string, error) {
 		return line, err
 	}
 
-    if line[0] == '+' {
-        return ParseSimpleString(line[1:]), nil        
-    } else if line[0] == '-' {
-        return ParseError(line[1:]), nil
-    } else if line[0] == ':' {
-        return ParseInteger(line[1:]), nil
-    } else if line[0] == '$' {
-        res, errBulk := ProcessBulkString(context, line[1:])
-        if errBulk != nil {
-            return res, errBulk
-        } else {
-            return res, nil
-        }
-    } else if line[0] == '*' {
-        return "", nil
-    }
+	if line[0] == '+' {
+		return ParseSimpleString(line[1:]), nil
+	} else if line[0] == '-' {
+		return ParseError(line[1:]), nil
+	} else if line[0] == ':' {
+		return ParseInteger(line[1:]), nil
+	} else if line[0] == '$' {
+		res, errBulk := ProcessBulkString(context, line[1:])
+		if errBulk != nil {
+			return res, errBulk
+		} else {
+			return res, nil
+		}
+	} else if line[0] == '*' {
+		return "", nil
+	}
 	return "", nil
 }
 
+/*
+ * process the bulk string
+ */
 func ProcessBulkString(ctx *RedisContext, line string) (string, error) {
 	intBulkLen := parseLen(line)
 	if intBulkLen > 0 {
 		var strNewLine = ReadLine(ctx)
 		intCrlfPos := strings.Index(strNewLine, "\r\n")
 		return fmt.Sprintf("\"%s\"", strNewLine[:intCrlfPos]), nil
-	} else if intBulkLen == 0{
+	} else if intBulkLen == 0 {
 		return "", nil
 	} else {
-        return "nil", nil
-    }
+		return "nil", nil
+	}
 }
 
+/*
+ * get the length of the bulk string
+ */
 func parseLen(line string) int {
-    strArray := strings.Split(line, "\r\n")
-    intLen, _ := strconv.Atoi(strArray[0])
-    return intLen
+	strArray := strings.Split(line, "\r\n")
+	intLen, _ := strconv.Atoi(strArray[0])
+	return intLen
 }
 
+/*
+ * check the prefix is valid
+ */
 func isValidPrefix(prefix byte) bool {
 	switch prefix {
-        case '-' :
-        case '$' :
-        case '*' :
-        case '+' :
-        case ':' : return true;
-        default: return false; 
-    }
-    return false
+	case '-':
+	case '$':
+	case '*':
+	case '+':
+	case ':':
+		return true
+	default:
+		return false
+	}
+	return false
 }

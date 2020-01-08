1312e6be05457b81d75609764cca253963dfe666
donghao02 <donghao02@baidu.com>
2020-01-08 11:10:39 +0800

    add some comments for reader

--- context.go
+++ context.go
@@ -1,38 +1,51 @@
 package rediscli
 
 import (
-    "bufio"
-    "net"
+	"bufio"
+	"net"
 )
 
 type RedisContext struct {
-    ip      string
-    port    string
-    conn    net.Conn
-    reader  *bufio.Reader
-    command string
+	ip      string
+	port    string
+	conn    net.Conn
+	reader  *bufio.Reader
+	command string
 }
 
+/**
+ * brief: get the redis context
+ * ip: redis server ip
+ * port: redis server port
+ */
 func GetRedisContext(ip string, port string) *RedisContext {
-    var ctx = initContext(ip, port)
-    _, err := ConnectToServer(ctx)
-    if err != nil {
-        return nil
-    }
-    setContextReader(ctx)
-    return ctx
+	var ctx = initContext(ip, port)
+	_, err := ConnectToServer(ctx)
+	if err != nil {
+		return nil
+	}
+	setContextReader(ctx)
+	return ctx
 }
 
+/**
+ * @brief: set reader for redis contexts
+ */
 func setContextReader(ctx *RedisContext) {
-    ctx.reader = bufio.NewReader(ctx.conn)
+	ctx.reader = bufio.NewReader(ctx.conn)
 }
 
+/**
+ * brief: init the redis context
+ * ip: redis server ip
+ * port: redis server port
+ */
 func initContext(ip string, port string) *RedisContext {
-    var ctx RedisContext
-    ctx.ip = ip
-    ctx.port = port
-    ctx.conn = nil
-    ctx.reader = nil
-    ctx.command = ""
-    return &ctx
+	var ctx RedisContext
+	ctx.ip = ip
+	ctx.port = port
+	ctx.conn = nil
+	ctx.reader = nil
+	ctx.command = ""
+	return &ctx
 }

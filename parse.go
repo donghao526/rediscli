4b267afed7b586ea9488a639eb0df5ba201e5c4e
donghao526 <donghao526@gmail.com>
2020-05-15 18:43:20 +0800

    add split args

--- parse.go
+++ parse.go
@@ -64,3 +64,94 @@ func parseArray(reply *RedisObject) string {
 	}
 	return out
 }
+
+func isSpace(char byte) bool {
+	if char == ' ' {
+		return true
+	} else {
+		return false
+	}
+}
+
+func splitArgs(line string) []string {
+	var args []string
+	argc := 0
+	p := 0
+	length := len(line)
+	for true {
+		/* skip blanks */
+		for p < length && isSpace(line[p]) {
+			p++
+		}
+
+		if p < length {
+			indq := false
+			insq := false
+			done := false
+
+			current := ""
+
+			for !done {
+				if indq == true {
+					if line[p] == '\\' && line[p+1] == 'x' {
+						p += 3
+					} else if line[p] == '\\' && p + 1 < length {
+						p++
+						switch line[p] {
+						case 'n': current += string('\n')
+						case 'r': current += string('\r')
+						case 't': current += string('\t')
+						case 'b': current += string('\b')
+						case 'a': current += string('\a')
+						default:
+							current += string(line[p])
+						}
+					} else if line[p] == '"' {
+						if p + 1 < length && !isSpace(line[p+1]) {
+							done = true
+						}
+					} else if p >= length {
+						goto err
+					} else {
+					}
+				} else if insq == true {
+					if line[p] == '\\' && line[p+1] == '\'' {
+						p++
+						current += "'"
+					} else if line[p] == '\'' {
+						/* closing quote must be followed by a space or
+						 * nothing at all. */
+						if p + 1 < length && !isSpace(line[p+1]) {
+							goto err
+						}
+						done = true
+					} else if p >= length {
+						/* unterminated quotes */
+						goto err
+					} else {
+						current += string(line[p])
+					}
+				} else {
+					switch line[p] {
+					case ' ': fallthrough
+					case '\n': fallthrough
+					case '\r': fallthrough
+					case '\t': fallthrough
+					case '0':
+						done = true
+					case '"':
+						indq = true
+					case '\'':
+						insq = true
+					default:
+						current = string(line[p])
+					}
+				}
+				p++
+			}
+		}
+	}
+
+err:
+	return nil
+}
